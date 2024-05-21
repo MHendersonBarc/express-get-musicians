83ceb63d57c29bd80858a9943bcd2003bc00d448
MHendersonBarc <michael.henderson@barclays.com>
2024-05-21 16:23:06 +0100

    Step 1 and 2 done

--- index.test.js
+++ index.test.js
@@ -12,11 +12,29 @@ const seedMusician = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test("Testing musicians endpoint", async () => {
+        // Sends request to `musicians` endpoint
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+
+        expect(response.statusCode).toBe(200)
+        expect(responseData[1].name).toBe(`Drake`)
+    })
     
-    
+    test("Testing musicians endpoint with id", async () => {
+        // Sends request to `musicians` endpoint
+        const response = await request(app).get("/musicians/1");
+        const response2 = await request(app).get("/musicians/2");
+        const response3 = await request(app).get("/musicians/3");
+        const responseData = JSON.parse(response.text);
+        const responseData2 = JSON.parse(response2.text);
+        const responseData3 = JSON.parse(response3.text);
 
-
-
-
-    
+        expect(response.statusCode).toBe(200);
+        expect(response2.statusCode).toBe(200);
+        expect(response3.statusCode).toBe(200);
+        expect(responseData.name).toBe(`Mick Jagger`);
+        expect(responseData2.instrument).toBe(`Voice`);
+        expect(responseData3.id).toBe(3);
+    })
 })
--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get(`/musicians`, async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+});
+
+app.get(`/musicians/:id`, async (req, res) =>{
+    const musician = await Musician.findByPk(req.params.id);
+    res.json(musician);
+});
+
+app.post(`/`, async (req, res) => {
+    const newMusician = await Musician.create({
+        name: req.body.name,
+        instrument: req.body.instrument,
+    })
+    res.json(`Added ${newMusician}`)
+});
+
+app.put(``);
+
+app.delete(``);
 
 
 
